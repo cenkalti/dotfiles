2eeb2861b211acd86a041cb499990d0b818c6472
Cenk Alti <cenkalti@gmail.com>
2011-09-24 12:35:02 +0300

    changed qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,118 +1,165 @@
-from libqtile.manager import Key, Screen, Group
+# -*- coding: utf-8 -*-
+from libqtile.manager import Key, Click, Drag, Screen, Group
 from libqtile.command import lazy
-from libqtile import layout, bar, widget
+from libqtile import layout, bar, widget, hook
 
-# The bindings below are for use with a Kinesis keyboard, and may not make
-# sense for standard keyboards.
+from libqtile import xcbq
+xcbq.keysyms["XF86AudioRaiseVolume"] = 0x1008ff13
+xcbq.keysyms["XF86AudioLowerVolume"] = 0x1008ff11
+xcbq.keysyms["XF86AudioMute"] = 0x1008ff12
+
+mod = "mod4"
 keys = [
-    # First, a set of bindings to control the layouts
-    Key(
-        ["mod1"], "k",
-        lazy.layout.down()
-    ),
-    Key(
-        ["mod1"], "j",
-        lazy.layout.up()
-    ),
-    Key(
-        ["mod1", "control"], "k",
-        lazy.layout.shuffle_down()
-    ),
-    Key(
-        ["mod1", "control"], "j",
-        lazy.layout.shuffle_up()
-    ),
-    Key(
-        ["mod1"], "space",
-        lazy.layout.next()
-    ),
-    Key(
-        ["mod1", "shift"], "space",
-        lazy.layout.rotate()
-    ),
-    Key(
-        ["mod1", "shift"], "Return",
-        lazy.layout.toggle_split()
-    ),
+    Key([mod], "j",
+        lazy.layout.down()),
+    Key([mod], "k",
+        lazy.layout.up()),
+    Key([mod, "shift"], "j",
+        lazy.layout.move_down()),
+    Key([mod, "shift"], "k",
+        lazy.layout.move_up()),
+    Key([mod, "control"], "j",
+        lazy.layout.section_down()),
+    Key([mod, "control"], "k",
+        lazy.layout.section_up()),
+    Key([mod], "h",
+        lazy.layout.collapse_branch()),  # for tree layout
+    Key([mod], "l",
+        lazy.layout.expand_branch()),  # for tree layout
+    Key([mod, "shift"], "h",
+        lazy.layout.move_left()),
+    Key([mod, "shift"], "l",
+        lazy.layout.move_right()),
+    Key([mod, "control"], "l",
+        lazy.layout.increase_ratio()),
+    Key([mod, "control"], "h",
+        lazy.layout.decrease_ratio()),
+    Key([mod], "comma",
+        lazy.layout.increase_nmaster()),
+    Key([mod], "period",
+        lazy.layout.decrease_nmaster()),
+    Key([mod], "Tab",
+        lazy.group.next_window()),
+    Key([mod, "shift"], "Tab",
+        lazy.group.prev_window()),
+    Key([mod, "shift"], "Return",
+        lazy.layout.rotate()),
+    Key([mod, "shift"], "space",
+        lazy.layout.toggle_split()),
 
-    Key(["mod1"], "n",      lazy.spawn("firefox")),
-    Key(["mod1"], "h",      lazy.to_screen(1)),
-    Key(["mod1"], "l",      lazy.to_screen(0)),
-    # ~/bin/x starts a terminal program
-    Key(["mod1"], "Return", lazy.spawn("/usr/bin/xfterm4")),
-    Key(["mod1"], "Tab",    lazy.nextlayout()),
-    Key(["mod1"], "w",      lazy.window.kill()),
+    Key([mod], "w",
+        lazy.to_screen(0)),
+    Key([mod], "e",
+        lazy.to_screen(1)),
+    Key([mod], "space",
+        lazy.nextlayout()),
+    Key([mod], "c",
+        lazy.window.kill()),
+    Key([mod], "t",
+        lazy.window.disable_floating()),
+    Key([mod, "shift"], "t",
+        lazy.window.enable_floating()),
+    Key([mod], "p",
+        lazy.spawn("exec dmenu_run "
+            "-fn 'Consolas:size=13' -nb '#000000' -nf '#ffffff' -b")),
+    Key([mod], "q",
+        lazy.spawn('xtrlock')),
+    Key([mod, 'shift', 'control'], "s",
+        lazy.spawn('/home/cenk/bin/synergy.sh')),
 
-    # The bindings below control Amarok, and my sound volume.
-    Key(
-        ["mod1", "shift"], "k",
-        lazy.spawn("amixer -c 1 -q set Speaker 2dB+")
-    ),
-    Key(
-        ["mod1", "shift"], "j",
-        lazy.spawn("amixer -c 1 -q set Speaker 2dB-")
-    ),
-    Key(
-        ["mod1", "shift"], "n",
-        lazy.spawn("amarok -t")
-    ),
-    Key(
-        ["mod1", "shift"], "l",
-        lazy.spawn("amarok -f")
-    ),
-    Key(
-        ["mod1", "shift"], "h",
-        lazy.spawn("amarok -r")
-    ),
+    Key([], "XF86AudioRaiseVolume",
+        lazy.spawn("amixer sset Master 5%+")),
+    Key([], "XF86AudioLowerVolume",
+        lazy.spawn("amixer sset Master 5%-")),
+    Key([], "XF86AudioMute",
+        lazy.spawn("amixer sset Master toggle")),
+    Key(["shift"], "XF86AudioRaiseVolume",
+        lazy.spawn("mpc volume +5")),
+    Key(["shift"], "XF86AudioLowerVolume",
+        lazy.spawn("mpc volume -5")),
+    Key(["shift"], "XF86AudioMute",
+        lazy.spawn("mpc toggle")),
+
+    Key([mod], "Left",
+        lazy.prevgroup()),
+    Key([mod], "Right",
+        lazy.nextgroup()),
 ]
 
-# Next, we specify group names, and use the group name list to generate an appropriate
-# set of bindings for group switching.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+        start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+        start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+border = dict(
+    border_normal='#808080',
+    border_width=2,
+    )
+
+layouts = [
+    layout.Tile(**border),
+    layout.Max(),
+    layout.Stack(**border),
+    layout.TreeTab(sections=['Surfing', 'E-mail', 'Docs', 'Incognito']),
+    layout.Slice('left', 320, wmclass='pino',
+        fallback=layout.Slice('right', 320, role='roster',
+        fallback=layout.Stack(1, **border))),
+    layout.Slice('left', 192, role='gimp-toolbox',
+        fallback=layout.Slice('right', 256, role='gimp-dock',
+        fallback=layout.Stack(1, **border))),
+    ]
+floating_layout = layout.Floating(**border)
+
 groups = [
-    Group("a"),
-    Group("s"),
-    Group("d"),
-    Group("f"),
-    Group("u"),
-    Group("i"),
-    Group("o"),
-    Group("p"),
-]
+    Group('1'),
+    Group('2'),
+    Group('3'),
+    Group('4'),
+    Group('5'),
+    Group('6'),
+    Group('7'),
+    Group('8'),
+    Group('9'),
+    ]
+
 for i in groups:
     keys.append(
-        Key(["mod1"], i.name, lazy.group[i.name].toscreen())
+        Key([mod], i.name, lazy.group[i.name].toscreen())
     )
     keys.append(
-        Key(["mod1", "shift"], i.name, lazy.window.togroup(i.name))
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name))
     )
 
-
-# Two simple layout instances:
-layouts = [
-    layout.Max(),
-    layout.Stack(stacks=2)
-]
-
-
-# I have two screens, each of which has a Bar at the bottom. Each Bar has two
-# simple widgets - a GroupBox, and a WindowName.
 screens = [
     Screen(
-        bottom = bar.Bar(
+        top = bar.Bar(
                     [
-                        widget.GroupBox(),
-                        widget.WindowName()
+                        widget.GroupBox(borderwidth=2,
+                            font='Consolas',fontsize=18,
+                            padding=1, margin_x=1, margin_y=1),
+                        widget.Sep(),
+                        widget.WindowName(
+                            font='Consolas',fontsize=18, margin_x=6),
+                        widget.Sep(),
+                        widget.CPUGraph(),
+                        widget.MemoryGraph(),
+                        widget.SwapGraph(foreground='C02020'),
+                        widget.Sep(),
+                        widget.Systray(),
+                        widget.Sep(),
+                        widget.Clock('%H:%M %d/%m',
+                            font='Consolas', fontsize=18, padding=6),
                     ],
-                    30,
+                    24,
                 ),
     ),
-    Screen(
-        bottom = bar.Bar(
-                    [
-                        widget.GroupBox(),
-                        widget.WindowName()
-                    ],
-                    30,
-                ),
-    )
 ]
+
+@hook.subscribe.client_new
+def dialogs(window):
+    if(window.window.get_wm_type() == 'dialog'
+        or window.window.get_wm_transient_for()):
+        window.floating = True
