dbb20774d049e283cde7ab2c11fdd77a2ffa77dd
Cenk Alti <cenkalti@gmail.com>
2014-04-12 15:46:57 -0700

    refactor xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,63 +1,47 @@
 import XMonad
-import XMonad.Core
-
 import XMonad.Prompt
 import XMonad.Prompt.Shell
-import XMonad.Prompt.Man
-
-import XMonad.Layout
-import XMonad.Layout.NoBorders
-import XMonad.Layout.ResizableTile
-
 import XMonad.Hooks.ManageDocks
-
-import XMonad.Util.EZConfig
 import XMonad.Util.Run
 import qualified Data.Map as M
-import System.IO
 
 main = do
-   xmproc <- spawnPipe "xmobar"
-   xmonad $ defaultConfig
-      { terminal = "terminator"
-      , focusedBorderColor = myActiveBorderColor
-      , modMask = mod4Mask
-      , keys = myKeys
-      , borderWidth = 2
-      , layoutHook = avoidStruts $ myLayoutHook
-     }   
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { terminal           = "terminator"
+        , focusedBorderColor = "#C80003"
+        , modMask            = mod4Mask
+        , keys               = myKeys
+        , borderWidth        = 2
+        , layoutHook         = myLayout
+        }
 
--- Layouts
-myLayoutHook = smartBorders $ (tiled ||| Mirror tiled ||| Full)
-  where
-      tiled = ResizableTall nmaster delta ratio []
-      nmaster = 1
-      delta = 3/100
-      ratio = 1/2
-
--- Colors
-myFgColor = "white"
-myBgColor = "#099999"
-myHighlightedFgColor = "white"
-myHighlightedBgColor = "#FF3300"
-myActiveBorderColor = "#C80003"
+-- The available layouts.  Note that each layout is separated by |||, which
+-- denotes layout choice.
+myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
+    where
+        -- default tiling algorithm partitions the screen into two panes
+        tiled   = Tall nmaster delta ratio
+        -- The default number of windows in the master pane
+        nmaster = 1
+        -- Default proportion of screen occupied by master pane
+        ratio   = 0.618
+        -- Percent of screen to increment by when resizing panes
+        delta   = 3/100
 
 -- Prompt config
-myXPConfig = defaultXPConfig {
-  position = Top,
-  promptBorderWidth = 0,
-  height = 15,
-  bgColor = myBgColor,
-  fgColor = myFgColor,
-  fgHLight = myHighlightedFgColor,
-  bgHLight = myHighlightedBgColor
-  }
+myXPConfig = defaultXPConfig
+    { font              = "-misc-fixed-Medium-*-*-*-20-*-*-*-*-*-*-*"
+    , position          = Top
+    , promptBorderWidth = 0
+    , height            = 20
+    }
 
 -- Union default and new key bindings
-myKeys x  = M.union (M.fromList (newKeys x)) (keys defaultConfig x)
+myKeys x = M.union (M.fromList (newKeys x)) (keys defaultConfig x)
 
 -- Add new and/or redefine key bindings
 newKeys conf@(XConfig {XMonad.modMask = modm}) = [
-  -- Use shellPrompt instead of default dmenu
-  ((modm, xK_p), shellPrompt myXPConfig)
-   ]
+    -- Use shellPrompt instead of default dmenu
+    ((modm, xK_p), shellPrompt myXPConfig)
+    ]
