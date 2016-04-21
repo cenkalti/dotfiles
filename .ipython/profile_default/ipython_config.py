8576bb34c407d7d4fd9b086fd096dc1ac885eab6
Cenk Alti <cenkalti@gmail.com>
2016-04-21 17:01:41 +0300

    ipython: do not put newline before prompt

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -2,3 +2,4 @@ c = get_config()
 c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.editor = 'vim'
 c.TerminalInteractiveShell.autocall = 1
+c.TerminalInteractiveShell.separate_in = ''
