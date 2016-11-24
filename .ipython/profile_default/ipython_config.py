db99abf01bf1cbdf1c3193e548f685376cf0a4ed
Cenk Alti <cenkalti@gmail.com>
2016-11-24 18:34:31 +0300

    ipyton: disable autocall

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,5 +1,3 @@
 c = get_config()
 c.TerminalIPythonApp.display_banner = False
-c.TerminalInteractiveShell.editor = 'vim'
-c.TerminalInteractiveShell.autocall = 1
 c.TerminalInteractiveShell.separate_in = ''
