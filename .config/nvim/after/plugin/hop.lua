0ad4474f9835c1a7c0aab0f23be45689cebab20a
Cenk Alti <cenkalti@gmail.com>
2023-01-24 00:14:20 -0500

    vim: remove / hop key binding

--- .config/nvim/after/plugin/hop.lua
+++ .config/nvim/after/plugin/hop.lua
@@ -4,7 +4,3 @@ hop.setup()
 
 vim.keymap.set('', 's', hop.hint_char2, {remap=true})
 
-vim.keymap.set('', '/', function()
-    hop.hint_patterns()
-    vim.cmd("set hlsearch | let @/='\\<'.expand('<cword>').'\\>'")
-end, {remap=true})
