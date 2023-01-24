743bcb8013bd51524524dca29468342acc18b698
Cenk Alti <cenkalti@gmail.com>
2023-01-24 00:09:26 -0500

    vim: do not ignore gitignored files when searching through files

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,7 +2,9 @@ require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', function()
+  builtin.find_files({ no_ignore = true })
+end, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>a', function()
