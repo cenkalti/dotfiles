b4487d5f6cc7e51036b403833f594b7785ba5e3a
Cenk Alti <cenkalti@gmail.com>
2023-03-15 16:56:20 -0400

    vim: update GBrowse command

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -2,5 +2,5 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
 -- View http link for current buffer
-vim.keymap.set("n", "gb", vim.cmd.Gbrowse);
-vim.keymap.set("v", "gb", vim.cmd.Gbrowse);
+vim.keymap.set("n", "gb", vim.cmd.GBrowse);
+vim.keymap.set("v", "gb", vim.cmd.GBrowse);
