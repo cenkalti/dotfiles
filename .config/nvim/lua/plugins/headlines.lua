6bb551613de95486811d9643812b0ab97afb56b0
Cenk Alti <cenkalti@gmail.com>
2024-01-12 15:27:28 -0500

    vim: install headlines plugin for highlighting markdown

---
+++ .config/nvim/lua/plugins/headlines.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "lukas-reineke/headlines.nvim",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        opts = {
+            markdown = {
+                fat_headline_upper_string = "▄",
+                fat_headline_lower_string = "▀",
+            },
+        },
+    },
+}
