c2932c697e76ef21bdbdb9dc735aa80e4bc2237f
Cenk Altı <cenkalti@gmail.com>
2024-02-06 00:49:25 -0500

    vim: upgrade zero-lsp

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -1,7 +1,7 @@
 return {
   {
     'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
+    branch = 'v3.x',
     dependencies = {
       -- LSP Support
       {'neovim/nvim-lspconfig'},
@@ -18,9 +18,11 @@ return {
       {'folke/which-key.nvim'},
     },
     config = function ()
-      local lsp = require('lsp-zero').preset({})
+      local lsp_zero = require('lsp-zero')
+      local lspconfig = require('lspconfig')
+      local cmp = require('cmp')
 
-      lsp.set_preferences({
+      lsp_zero.set_preferences({
         -- I'll set my own key bindings
         set_lsp_keymaps = false,
 
@@ -29,10 +31,8 @@ return {
       })
 
       -- Setup LSP key bindings
-      lsp.on_attach(function(client, bufnr)
-        local wk = require("which-key")
-
-        wk.register({
+      lsp_zero.on_attach(function(client, bufnr)
+        require("which-key").register({
           K = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
           L = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
 
@@ -47,29 +47,48 @@ return {
 
       end)
 
+      --- if you want to know more about lsp-zero and mason.nvim
+      --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+      require('mason').setup({})
+      require('mason-lspconfig').setup({
+        ensure_installed = {},
+        handlers = {
+          lsp_zero.default_setup,
+          lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            lspconfig.lua_ls.setup(lua_opts)
+          end,
+        }
+      })
+
       -- Setup completion key bindings
-      local cmp = require('cmp')
-      lsp.setup_nvim_cmp({
-        mapping = {
-          ['<C-e>'] = cmp.mapping.complete(),
+      local cmp_action = lsp_zero.cmp_action()
+      cmp.setup({
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
           ['<Tab>'] = cmp.mapping.confirm(),
           ['<C-c>'] = cmp.mapping.close(),
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
-        },
+        })
       })
 
       -- Configure language servers
-      require('lspconfig').pylsp.setup{
+      lspconfig.pylsp.setup{
         settings = {
           pylsp = {
             configurationSources = {'flake8'},
           }
         }
       }
-
-      -- Setup lsp-zero after configuration
-      lsp.setup()
     end,
   },
 }
