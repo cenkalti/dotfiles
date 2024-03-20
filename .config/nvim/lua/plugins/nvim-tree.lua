75f6ca414e7de2417995c1b62aee11bef0399bef
Cenk Altı <cenkalti@gmail.com>
2024-03-20 16:28:50 -0400

    vim: do not close nvim automatically if no buffer is open other than nvim-tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -40,17 +40,6 @@ return {
         ["<Leader>r"] = { "<cmd>NvimTreeFindFile<CR>", "Find File in Nvim Tree" },
       })
 
-      -- Close the tab/nvim when nvim-tree is the last window.
-      -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#naive-solution
-      vim.api.nvim_create_autocmd("BufEnter", {
-        nested = true,
-        callback = function()
-          if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-            vim.cmd "quit"
-          end
-        end
-      })
-
     end,
   },
 }
