a98e2d72b655a6c17e80472fe0927575d4d096d6
Cenk Altı <cenkalti@gmail.com>
2024-03-27 16:58:48 -0400

    vim: show lsp diagnostic in tabs

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -23,6 +23,11 @@ return {
         end,
         show_close_icon = false,
         show_buffer_close_icons = false,
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
         offsets = {
           {
             filetype = "NvimTree",
