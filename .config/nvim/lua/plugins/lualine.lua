57db61f9048e23ecaf507e8ba38e3e8f5071f6e6
Cenk Altı <cenkalti@gmail.com>
2024-03-28 13:55:35 -0400

    vim: disable lualine on nvim-tree

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,11 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require('lualine').setup()
+      require('lualine').setup({
+        options = {
+          disabled_filetypes = { 'NvimTree' },
+        },
+      })
     end,
   },
 }
