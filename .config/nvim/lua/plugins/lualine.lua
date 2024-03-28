d0a795c410187b07c8f3851f9a5ab92376ebd69f
Cenk Altı <cenkalti@gmail.com>
2024-03-28 16:33:40 -0400

    vim: show current function name in status line

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,21 @@
 -- Status line
+
+local function truncate_string(str, maxLength)
+    if #str > maxLength then
+        return string.sub(str, 1, maxLength - 3) .. "..."
+    else
+        return str
+    end
+end
+
+local function tree_location()
+  local status_line = require('nvim-treesitter').statusline({
+    type_patterns = {'function', 'method'},
+    indicator_size = 1000, -- large number to disable
+  })
+  return truncate_string(status_line, 100)
+end
+
 return {
   {
     'nvim-lualine/lualine.nvim',
@@ -7,6 +24,9 @@ return {
       require('lualine').setup({
         options = {
           disabled_filetypes = { 'NvimTree' },
+        },
+        sections = {
+          lualine_c = {'filename', tree_location},
         },
       })
     end,
