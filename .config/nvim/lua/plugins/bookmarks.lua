ac9b6c29aec3a073a9312dc7f1339d4008ceb607
Cenk Altı <cenkalti@gmail.com>
2024-03-29 15:02:58 -0400

    vim: use global bookmarks

--- .config/nvim/lua/plugins/bookmarks.lua
+++ .config/nvim/lua/plugins/bookmarks.lua
@@ -2,7 +2,6 @@
 return {
   'mattesgroeger/vim-bookmarks',
   init = function ()
-    vim.g.bookmark_save_per_working_dir = 1
     vim.g.bookmark_auto_save = 1
     vim.g.bookmark_sign = '>>'
     vim.g.bookmark_annotation_sign = '##'
