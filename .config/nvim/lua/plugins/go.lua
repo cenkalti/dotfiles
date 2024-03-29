f48076ac835ba1edc95f49d68b557299da3c42df
Cenk Altı <cenkalti@gmail.com>
2024-03-29 15:02:23 -0400

    vim: update function call

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -13,7 +13,7 @@ return {
       vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = "*.go",
         callback = function()
-         require('go.format').goimport()
+         require('go.format').goimports()
         end,
         group = format_sync_grp,
       })
