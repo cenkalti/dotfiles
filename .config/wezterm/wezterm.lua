2562421b1e5c7900ef62c9e676887f12f9458c5f
Cenk Altı <cenkalti@gmail.com>
2024-04-15 18:34:35 -0400

    wez: keep window size when changing font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,6 +8,7 @@ config.color_scheme = 'nord'
 config.font_size = 13.0
 config.quit_when_all_windows_are_closed = false
 config.default_prog = { '/opt/homebrew/bin/zsh', '-li' }
+config.adjust_window_size_when_changing_font_size = false
 
 -- and finally, return the configuration to wezterm
 return config
