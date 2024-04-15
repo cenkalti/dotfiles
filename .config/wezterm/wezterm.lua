9eafe4a8e6c2ba195f50572ce445929eb962c66a
Cenk Altı <cenkalti@gmail.com>
2024-04-15 18:42:46 -0400

    wez: hide tab bar if only one tab

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,6 +9,7 @@ config.font_size = 13.0
 config.quit_when_all_windows_are_closed = false
 config.default_prog = { '/opt/homebrew/bin/zsh', '-li' }
 config.adjust_window_size_when_changing_font_size = false
+config.hide_tab_bar_if_only_one_tab = true
 
 -- and finally, return the configuration to wezterm
 return config
