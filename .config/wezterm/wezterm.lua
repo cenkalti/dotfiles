aedcc4d6302600f2a87c8797282c830c7ec0b370
Cenk Altı <cenkalti@gmail.com>
2024-04-15 18:45:05 -0400

    wez: set initial window size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,6 +10,8 @@ config.quit_when_all_windows_are_closed = false
 config.default_prog = { '/opt/homebrew/bin/zsh', '-li' }
 config.adjust_window_size_when_changing_font_size = false
 config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 120
+config.initial_rows = 36
 
 -- and finally, return the configuration to wezterm
 return config
