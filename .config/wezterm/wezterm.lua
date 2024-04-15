4eaf0cd1b1728f120bdbf4e134cf15dc46c15434
Cenk Altı <cenkalti@gmail.com>
2024-04-15 18:19:56 -0400

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.color_scheme = 'nord'
+config.font_size = 13.0
+config.quit_when_all_windows_are_closed = false
+config.default_prog = { '/opt/homebrew/bin/zsh', '-li' }
+
+-- and finally, return the configuration to wezterm
+return config
+
