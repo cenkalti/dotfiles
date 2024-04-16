31464fc778f5147e054dfdc878cf6ce3729b0bc1
Cenk Altı <cenkalti@gmail.com>
2024-04-16 12:22:33 -0400

    vim: replace bookmark plugin

--- .config/nvim/lua/plugins/bookmarks.lua
+++ .config/nvim/lua/plugins/bookmarks.lua
@@ -1,10 +1,30 @@
 -- Custom bookmarks
 return {
-  'mattesgroeger/vim-bookmarks',
-  init = function ()
-    vim.g.bookmark_auto_save = 1
-    vim.g.bookmark_sign = '>>'
-    vim.g.bookmark_annotation_sign = '##'
-    vim.g.bookmark_show_toggle_warning = 0
+  'tomasky/bookmarks.nvim',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    'folke/which-key.nvim',
+  },
+  config = function()
+    local bookmarks = require('bookmarks')
+    bookmarks.setup {
+      sign_priority = 8,  --set bookmark sign priority to cover git signs
+      keywords =  {
+        ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
+        ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
+        ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
+        ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
+      },
+    }
+    require('telescope').load_extension('bookmarks')
+    require("which-key").register({
+        ["mm"] = { bookmarks.bookmark_toggle, "Toggle Bookmark" },
+        ["mi"] = { bookmarks.bookmark_ann, "Add or Edit Bookmark Annotation" },
+        ["mc"] = { bookmarks.bookmark_clean, "Clean Bookmarks in Local Buffer" },
+        ["mn"] = { bookmarks.bookmark_next, "Next Bookmark" },
+        ["mp"] = { bookmarks.bookmark_prev, "Previous Bookmark" },
+        ["ml"] = { bookmarks.bookmark_list, "List Bookmarks" },
+        ["mL"] = { require('telescope').extensions.bookmarks.list, "List Bookmarks in Telescope" },
+    })
   end,
 }
