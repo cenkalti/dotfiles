f20864d39e8aa4e802989332d4b12e33b5d6faac
Cenk Altı <cenkalti@gmail.com>
2024-04-15 22:50:27 -0400

    vim: update harpoon config

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -9,30 +9,43 @@ return {
       harpoon:setup()
       -- REQUIRED
 
-
       -- basic telescope configuration
       local conf = require("telescope.config").values
       local function toggle_telescope(harpoon_files)
-          local file_paths = {}
-          for _, item in ipairs(harpoon_files.items) do
-              table.insert(file_paths, item.value)
+
+          local make_finder = function()
+              local paths = {}
+              for _, item in ipairs(harpoon_files.items) do
+                  table.insert(paths, item.value)
+              end
+              return require("telescope.finders").new_table({ results = paths })
           end
 
           require("telescope.pickers").new({}, {
               prompt_title = "Harpoon",
-              finder = require("telescope.finders").new_table({
-                  results = file_paths,
-              }),
+              finder = make_finder(),
               previewer = conf.file_previewer({}),
               sorter = conf.generic_sorter({}),
+              attach_mappings = function(prompt_buffer_number, map)
+                  map("n", "d", function()
+                      local state = require("telescope.actions.state")
+                      local selected_entry = state.get_selected_entry()
+                      local current_picker = state.get_current_picker(prompt_buffer_number)
+
+                      harpoon:list():remove_at(selected_entry.index)
+                      current_picker:refresh(make_finder())
+                  end)
+
+                  return true
+              end
           }):find()
+
       end
 
-      vim.keymap.set("n", "<leader>hh", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
-      vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-      vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
+      -- keybindings
+      require("which-key").register({
+          ["<leader>ha"] = { function() harpoon:list():add() end, "Add file to Harpoon" },
+          ["<leader>hh"] = { function() toggle_telescope(harpoon:list()) end, "Open Harpoon Window" },
+      })
     end,
 }
