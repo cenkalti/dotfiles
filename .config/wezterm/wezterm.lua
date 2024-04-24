0880fa294dff47f33f9c753a84e92d67207ff3c0
Cenk Altı <cenkalti@gmail.com>
2024-04-24 14:32:47 -0400

    wez: add launcher key

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,6 +13,10 @@ config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 120
 config.initial_rows = 36
 
+config.keys = {
+  { key = 'l', mods = 'SUPER', action = wezterm.action.ShowLauncher },
+}
+
 -- and finally, return the configuration to wezterm
 return config
 
