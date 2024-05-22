adb5292ee7ccec8b6ee70d6140ce469b032e3c3c
Cenk Altı <cenkalti@gmail.com>
2024-05-22 17:04:39 -0400

    vim: add linter setup

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -1,6 +1,12 @@
 return {
     'mfussenegger/nvim-lint',
     config = function()
+        require('lint').linters_by_ft = {
+            markdown = { 'vale' },
+            go = { 'golangcilint' },
+            python = { 'flake8', 'mypy' },
+            lua = { 'luacheck' },
+        }
         vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
             callback = function()
                 -- try_lint without arguments runs the linters defined in `linters_by_ft` for the current filetype
