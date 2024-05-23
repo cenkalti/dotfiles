644ac6510f69b39394a7fd89c5ff0ae088dba921
Cenk Altı <cenkalti@gmail.com>
2024-05-22 23:08:43 -0400

    vim: run mypy in virtual env

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -1,10 +1,16 @@
 return {
     'mfussenegger/nvim-lint',
     config = function()
+        local venv = os.getenv('VIRTUAL_ENV')
+        if venv then
+            require('lint').linters.mypy.args = { '--config-file', venv .. '/mypy.ini' }
+        end
+
         require('lint').linters_by_ft = {
             markdown = { 'vale' },
             python = { 'mypy' },
         }
+
         vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
             callback = function()
                 -- try_lint without arguments runs the linters defined in `linters_by_ft` for the current filetype
