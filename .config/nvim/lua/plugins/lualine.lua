ecb44d0ddbf649531f9b3cdec3200ced6a430d5e
Cenk Altı <cenkalti@gmail.com>
2024-05-29 15:42:00 -0400

    vim: show function name in status line

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,19 +1,33 @@
 -- Status line
 
-local function truncate_string(str, maxLength)
-    if #str > maxLength then
-        return string.sub(str, 1, maxLength - 3) .. '...'
-    else
-        return str
+local function get_function_name(line, node)
+    -- Ensure the node is either a function_declaration or method_declaration
+    if node:type() ~= 'function_declaration' and node:type() ~= 'method_declaration' then
+        return line
     end
+
+    -- Find the identifier child node
+    for i = 0, node:child_count() - 1 do
+        local child = node:child(i)
+        -- For Python
+        if child:type() == 'identifier' then
+            return vim.treesitter.get_node_text(child, 0)
+        end
+        -- For Go
+        if child:type() == 'field_identifier' then
+            return vim.treesitter.get_node_text(child, 0)
+        end
+    end
+
+    return line
 end
 
 local function tree_location()
-    local status_line = require('nvim-treesitter').statusline({
+    return require('nvim-treesitter').statusline({
         type_patterns = { 'function', 'method' },
-        indicator_size = 1000, -- large number to disable
+        transform_fn = get_function_name,
+        indicator_size = 40,
     })
-    return truncate_string(status_line, 100)
 end
 
 return {
@@ -24,11 +38,11 @@ return {
             require('lualine').setup({
                 options = {
                     globalstatus = true,
-                    component_separators = { left = '', right = '' },
                     section_separators = { left = '', right = '' },
+                    component_separators = { left = '|', right = '|' },
                 },
                 sections = {
-                    lualine_c = { 'filename', tree_location },
+                    lualine_c = { { 'filename', separator = '>' }, { tree_location } },
                     lualine_x = { 'filetype' },
                     lualine_y = {},
                 },
