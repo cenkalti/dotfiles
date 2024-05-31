0da067a48463040c1191a614cd630f4940a46e6b
Cenk Altı <cenkalti@gmail.com>
2024-05-31 09:48:55 -0400

    vim: fix search highlight

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,14 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
+        require('catppuccin').setup({
+            custom_highlights = function(colors)
+                return {
+                    Search = { bg = colors.red, fg = colors.mantle },
+                    CurSearch = { bg = colors.rosewater, fg = colors.mantle },
+                }
+            end,
+        })
         vim.cmd.colorscheme('catppuccin-frappe')
     end,
 }
