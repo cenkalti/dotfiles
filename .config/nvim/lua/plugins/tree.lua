8a4376c7f993a943f9a65a876214d890c96f257d
Cenk Altı <cenkalti@gmail.com>
2024-05-31 09:06:38 -0400

    vim: nvim tree auto close

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -44,6 +44,26 @@ return {
                 ['<Leader>n'] = { '<cmd>NvimTreeToggle<CR>', 'Toggle Nvim Tree' },
                 ['<Leader>r'] = { '<cmd>NvimTreeFindFile<CR>', 'Find File in Nvim Tree' },
             })
+
+            -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#marvinth01
+            vim.api.nvim_create_autocmd('QuitPre', {
+                callback = function()
+                    local invalid_win = {}
+                    local wins = vim.api.nvim_list_wins()
+                    for _, w in ipairs(wins) do
+                        local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+                        if bufname:match('NvimTree_') ~= nil then
+                            table.insert(invalid_win, w)
+                        end
+                    end
+                    if #invalid_win == #wins - 1 then
+                        -- Should quit, so we close all invalid windows.
+                        for _, w in ipairs(invalid_win) do
+                            vim.api.nvim_win_close(w, true)
+                        end
+                    end
+                end,
+            })
         end,
     },
 }
