aca9bdb168d46163a86148a886a031749872b169
Cenk Altı <cenkalti@gmail.com>
2024-06-04 07:59:14 -0400

    vim: add implement gpt hook

--- .config/nvim/lua/plugins/gp.lua
+++ .config/nvim/lua/plugins/gp.lua
@@ -32,6 +32,27 @@ return {
                         .. 'START AND END YOUR ANSWER WITH:\n\n```',
                 },
             },
+            hooks = {
+                -- GpImplement rewrites the provided selection/range based on comments in it
+                Implement = function(gp, params)
+                    local template = 'Having following from {{filename}}:\n\n'
+                        .. '```{{filetype}}\n{{selection}}\n```\n\n'
+                        .. 'Please rewrite this according to the contained instructions.'
+                        .. '\n\nRespond exclusively with the snippet that should replace the selection above.'
+
+                    local agent = gp.get_command_agent()
+                    gp.info('Implementing selection with agent: ' .. agent.name)
+
+                    gp.Prompt(
+                        params,
+                        gp.Target.rewrite,
+                        nil, -- command will run directly without any prompting for user input
+                        agent.model,
+                        template,
+                        agent.system_prompt
+                    )
+                end,
+            },
         })
 
         -- VISUAL mode mappings
