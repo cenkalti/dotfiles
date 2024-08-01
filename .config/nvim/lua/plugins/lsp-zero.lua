bd6c5eba73afd109f37126abf438b3a0d8d8ad24
Cenk Altı <cenkalti@gmail.com>
2024-07-31 22:08:22 -0400

    vim: switch to pyright from pylsp

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -78,6 +78,31 @@ return {
             -- Setup Mason package manager
             require('mason').setup({})
 
+            local function get_python_path(workspace)
+                local path = lspconfig.util.path
+
+                -- Use activated virtualenv.
+                if vim.env.VIRTUAL_ENV then
+                    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+                end
+
+                -- Find and use virtualenv in workspace directory.
+                local match = vim.fn.glob(path.join(workspace, '.venv', 'pyvenv.cfg'))
+                if match ~= '' then
+                    return path.join(path.dirname(match), 'bin', 'python')
+                end
+
+                -- Find and use virtualenv via poetry in workspace directory.
+                match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+                if match ~= '' then
+                    local venv = vim.fn.trim(vim.fn.system('poetry --directory ' .. workspace .. ' env info -p'))
+                    return path.join(venv, 'bin', 'python')
+                end
+
+                -- Fallback to system Python.
+                return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
+            end
+
             -- Configure LSP servers installed with Mason
             require('mason-lspconfig').setup({
                 ensure_installed = {},
@@ -85,9 +110,11 @@ return {
                 -- Custom setup for some LSP servers
                 handlers = {
                     lsp_zero.default_setup,
-                    pylsp = function()
-                        lspconfig.pylsp.setup({
-                            configurationSources = { 'pycodestyle' },
+                    pyright = function()
+                        lspconfig.pyright.setup({
+                            before_init = function(_, config)
+                                config.settings.python.pythonPath = get_python_path(config.root_dir)
+                            end,
                         })
                     end,
                     gopls = function()
