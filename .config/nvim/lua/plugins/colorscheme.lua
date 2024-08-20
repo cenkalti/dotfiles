3342ff4d78ac4d3d1938ef6fa3fc6c7c4f7de902
Cenk Altı <cenkalti@gmail.com>
2024-08-20 11:18:52 -0400

    vim: dim inactive window

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,11 @@ return {
     config = function()
         require('catppuccin').setup({
             flavour = 'macchiato',
+            dim_inactive = {
+                enabled = true,
+                shade = 'dark',
+                percentage = 0.15,
+            },
         })
         vim.cmd.colorscheme('catppuccin')
     end,
