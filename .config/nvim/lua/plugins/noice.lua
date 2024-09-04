e10ee309e2ef2f5dc890250ddb206a84eb394fcb
Cenk Altı <cenkalti@gmail.com>
2024-09-04 13:13:11 -0400

    vim: disable notify

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,6 +1,9 @@
 -- Highly experimental plugin that completely replaces the UI for messages, cmdline and the popupmenu.
 return {
     'folke/noice.nvim',
+    dependencies = {
+        'MunifTanjim/nui.nvim',
+    },
     event = 'VeryLazy',
     init = function()
         -- noice.nvim gives warning if this is enabled.
@@ -25,13 +28,5 @@ return {
             inc_rename = false, -- enables an input dialog for inc-rename.nvim
             lsp_doc_border = false, -- add a border to hover docs and signature help
         },
-    },
-    dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        'MunifTanjim/nui.nvim',
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        'rcarriga/nvim-notify',
     },
 }
