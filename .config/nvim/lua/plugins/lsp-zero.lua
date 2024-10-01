72bc06237e095bdc2306a48344dd88222d69037c
Cenk Altı <cenkalti@gmail.com>
2024-10-01 16:34:45 -0400

    vim: upgrade lsp-zero

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -1,11 +1,11 @@
 -- Created with following tutorial:
--- https://lsp-zero.netlify.app/v3.x/tutorial.html
+-- https://lsp-zero.netlify.app/v4.x/tutorial.html
 return {
     {
         -- LSP zero is collection of functions that will help you setup Neovim's LSP client,
         -- so you can get IDE-like features with minimum effort.
         'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
+        branch = 'v4.x',
 
         dependencies = {
             -- Contains LSP client configurations for various language servers
@@ -30,37 +30,14 @@ return {
             { 'folke/which-key.nvim' },
         },
 
-        -- Temporary fix for following error:
-        -- LSP[gopls]: Error SERVER_REQUEST_HANDLER_ERROR:
-        -- "...llar/neovim/0.10.0/share/nvim/runtime/lua/vim/_watch.lua:99: ENOENT: no such file or directory"
-        -- Issue: https://github.com/neovim/neovim/issues/28058
-        -- Fix: https://github.com/SuperAPPKid/astro_nvim_config/commit/3b9d7bb5f68dd468644a8ec4b4f8c716e45d7ae8
-        init = function()
-            local lsp = vim.lsp
-            local make_client_capabilities = lsp.protocol.make_client_capabilities
-            function lsp.protocol.make_client_capabilities()
-                local caps = make_client_capabilities()
-                caps.workspace.didChangeWatchedFiles = nil
-
-                return caps
-            end
-        end,
-
         config = function()
             local lsp_zero = require('lsp-zero')
             local lspconfig = require('lspconfig')
             local cmp = require('cmp')
 
-            lsp_zero.set_preferences({
-                -- I'll set my own key bindings
-                set_lsp_keymaps = false,
-
-                -- I'll manage LSP servers manually using :Mason
-                suggest_lsp_servers = false,
-            })
-
-            -- Setup LSP key bindings
-            lsp_zero.on_attach(function(client, bufnr)
+            -- lsp_attach is where you enable features that only work
+            -- if there is a language server active in the file
+            local lsp_attach = function(_, bufnr)
                 require('which-key').register({
                     K = { '<cmd>lua vim.lsp.buf.hover()<CR>', 'Hover' },
                     L = { '<cmd>lua vim.lsp.buf.signature_help()<CR>', 'Signature Help' },
@@ -70,38 +47,20 @@ return {
                     gt = { '<cmd>lua vim.lsp.buf.type_definition()<CR>', 'Go to Type Definition' },
                     gi = { '<cmd>lua vim.lsp.buf.implementation()<CR>', 'Go to Implementation' },
                     gR = { '<cmd>lua vim.lsp.buf.references()<CR>', 'List References' },
+                    gC = { '<cmd>lua vim.lsp.buf.code_action()<CR>', 'Code Action' },
 
                     ['<localleader>r'] = { '<cmd>lua vim.lsp.buf.rename()<CR>', 'Rename' },
                 }, { buffer = bufnr })
-            end)
+            end
+
+            lsp_zero.extend_lspconfig({
+                sign_text = true,
+                lsp_attach = lsp_attach,
+                capabilities = require('cmp_nvim_lsp').default_capabilities(),
+            })
 
             -- Setup Mason package manager
             require('mason').setup({})
-
-            local function get_python_path(workspace)
-                local path = lspconfig.util.path
-
-                -- Use activated virtualenv.
-                if vim.env.VIRTUAL_ENV then
-                    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
-                end
-
-                -- Find and use virtualenv in workspace directory.
-                local match = vim.fn.glob(path.join(workspace, '.venv', 'pyvenv.cfg'))
-                if match ~= '' then
-                    return path.join(path.dirname(match), 'bin', 'python')
-                end
-
-                -- Find and use virtualenv via poetry in workspace directory.
-                match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
-                if match ~= '' then
-                    local venv = vim.fn.trim(vim.fn.system('poetry --directory ' .. workspace .. ' env info -p'))
-                    return path.join(venv, 'bin', 'python')
-                end
-
-                -- Fallback to system Python.
-                return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
-            end
 
             -- Configure LSP servers installed with Mason
             require('mason-lspconfig').setup({
@@ -109,19 +68,23 @@ return {
 
                 -- Custom setup for some LSP servers
                 handlers = {
-                    -- lsp_zero.default_setup,  -- disabled because of following issue
-                    -- https://github.com/neovim/nvim-lspconfig/pull/3232#issuecomment-2333481121
+                    -- lsp_zero.default_setup,
                     function(server_name)
-                        if server_name == 'tsserver' then
-                            server_name = 'ts_ls'
-                        else
-                            lsp_zero.default_setup(server_name)
-                        end
+                        lspconfig[server_name].setup({})
+                    end,
+                    -- Custom setup for other LSP servers
+                    lua_ls = function()
+                        lspconfig.lua_ls.setup({
+                            on_init = function(client)
+                                lsp_zero.nvim_lua_settings(client, {})
+                            end,
+                        })
                     end,
                     pyright = function()
                         lspconfig.pyright.setup({
                             before_init = function(_, config)
-                                config.settings.python.pythonPath = get_python_path(config.root_dir)
+                                config.settings.python.pythonPath =
+                                    require('lib/python').get_python_path(config.root_dir)
                             end,
                         })
                     end,
