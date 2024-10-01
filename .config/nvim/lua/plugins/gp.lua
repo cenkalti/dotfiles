df5e438d7ad46165ad073096b0d9d923272498f6
Cenk Altı <cenkalti@gmail.com>
2024-10-01 15:54:07 -0400

    vim: update gp.nvim plugin config

--- .config/nvim/lua/plugins/gp.lua
+++ .config/nvim/lua/plugins/gp.lua
@@ -1,109 +1,86 @@
 return {
     'robitx/gp.nvim',
-    version = '3.5.1',
     config = function()
         require('gp').setup({
-            agents = {
-                -- Remove default agents
-                { name = 'ChatGPT3-5', disable = true },
-                { name = 'CodeGPT3-5', disable = true },
-
-                {
-                    name = 'ChatGPT4',
-                    chat = true,
-                    command = false,
-                    model = { model = 'gpt-4o', temperature = 0.2, top_p = 0.1 },
-                    system_prompt = 'You are a general AI assistant.\n\n'
-                        .. 'The user provided the additional info about how they would like you to respond:\n\n'
-                        .. "- If you're unsure don't guess and say you don't know instead.\n"
-                        .. '- Ask question if you need clarification to provide better answer.\n'
-                        .. '- Think deeply and carefully from first principles step by step.\n'
-                        .. '- Zoom out first to see the big picture and then zoom in to details.\n'
-                        .. '- Use Socratic method to improve your thinking and coding skills.\n'
-                        .. "- Don't elide any code from your output if the answer requires coding.\n"
-                        .. "- Take a deep breath; You've got this!\n",
-                },
-                {
-                    name = 'CodeGPT4',
-                    chat = false,
-                    command = true,
-                    model = { model = 'gpt-4o', temperature = 0.2, top_p = 0.1 },
-                    system_prompt = 'You are an AI working as a code editor.\n\n'
-                        .. 'Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n'
-                        .. 'START AND END YOUR ANSWER WITH:\n\n```',
-                },
-            },
+            default_chat_agent = 'ChatGPT4o',
+            default_command_agent = 'CodeGPT4o',
             hooks = {
-                Implement = function(gp, params)
-                    local template = 'Having following from {{filename}}:\n\n'
-                        .. '```{{filetype}}\n{{selection}}\n```\n\n'
-                        .. 'Please rewrite this according to the contained instructions.'
-                        .. '\n\nRespond exclusively with the snippet that should replace the selection above.'
-                    local agent = gp.get_command_agent()
-                    gp.Prompt(params, gp.Target.rewrite, nil, agent.model, template, agent.system_prompt)
-                end,
                 UnitTests = function(gp, params)
                     local template = 'I have the following code from {{filename}}:\n\n'
                         .. '```{{filetype}}\n{{selection}}\n```\n\n'
                         .. 'Please respond by writing table driven unit tests for the code above.'
                     local agent = gp.get_command_agent()
-                    gp.Prompt(params, gp.Target.enew, nil, agent.model, template, agent.system_prompt)
+                    gp.Prompt(params, gp.Target.enew, agent, template)
                 end,
                 Explain = function(gp, params)
                     local template = 'I have the following code from {{filename}}:\n\n'
                         .. '```{{filetype}}\n{{selection}}\n```\n\n'
                         .. 'Please respond by explaining the code above.'
                     local agent = gp.get_chat_agent()
-                    gp.Prompt(params, gp.Target.popup, nil, agent.model, template, agent.system_prompt)
+                    gp.Prompt(params, gp.Target.popup, agent, template)
+                end,
+                FormatString = function(gp, params)
+                    local template = 'I have the following code from {{filename}}:\n\n'
+                        .. '```{{filetype}}\n{{selection}}\n```\n\n'
+                        .. 'Please respond by reformatting the string above. Do not exceed 120 characters per line.'
+                    local agent = gp.get_command_agent()
+                    gp.Prompt(params, gp.Target.rewrite, agent, template)
                 end,
             },
         })
-
-        -- VISUAL mode mappings
-        require('which-key').register({
-            c = { ":<C-u>'<,'>GpChatNew<cr>", 'Visual Chat New' },
-            p = { ":<C-u>'<,'>GpChatPaste<cr>", 'Visual Chat Paste' },
-            t = { ":<C-u>'<,'>GpChatToggle<cr>", 'Visual Toggle Chat' },
-
-            r = { ":<C-u>'<,'>GpRewrite<cr>", 'Visual Rewrite' },
-            a = { ":<C-u>'<,'>GpAppend<cr>", 'Visual Append (after)' },
-            b = { ":<C-u>'<,'>GpPrepend<cr>", 'Visual Prepend (before)' },
-            i = { ":<C-u>'<,'>GpImplement<cr>", 'Implement selection' },
-            u = { ":<C-u>'<,'>GpUnitTests<cr>", 'Write Unit Tests' },
-            e = { ":<C-u>'<,'>GpExplain<cr>", 'Explain selection' },
-
-            n = { '<cmd>GpNextAgent<cr>', 'Next Agent' },
-            s = { '<cmd>GpStop<cr>', 'GpStop' },
-            x = { ":<C-u>'<,'>GpContext<cr>", 'Visual GpContext' },
-        }, {
-            mode = 'v',
-            prefix = '<C-g>',
-            buffer = nil,
-            silent = true,
-            noremap = true,
-            nowait = true,
-        })
-
-        -- NORMAL mode mappings
-        require('which-key').register({
-            c = { '<cmd>GpChatNew<cr>', 'New Chat' },
-            t = { '<cmd>GpChatToggle<cr>', 'Toggle Chat' },
-            f = { '<cmd>GpChatFinder<cr>', 'Chat Finder' },
-
-            r = { '<cmd>GpRewrite<cr>', 'Inline Rewrite' },
-            a = { '<cmd>GpAppend<cr>', 'Append (after)' },
-            b = { '<cmd>GpPrepend<cr>', 'Prepend (before)' },
-
-            n = { '<cmd>GpNextAgent<cr>', 'Next Agent' },
-            s = { '<cmd>GpStop<cr>', 'GpStop' },
-            x = { '<cmd>GpContext<cr>', 'Toggle GpContext' },
-        }, {
-            mode = 'n',
-            prefix = '<C-g>',
-            buffer = nil,
-            silent = true,
-            noremap = true,
-            nowait = true,
+        require('which-key').add({
+            -- VISUAL mode mappings
+            {
+                mode = { 'v' },
+                nowait = true,
+                remap = false,
+                { '<C-g><C-t>', ":<C-u>'<,'>GpChatNew tabnew<cr>", desc = 'ChatNew tabnew' },
+                { '<C-g><C-v>', ":<C-u>'<,'>GpChatNew vsplit<cr>", desc = 'ChatNew vsplit' },
+                { '<C-g><C-x>', ":<C-u>'<,'>GpChatNew split<cr>", desc = 'ChatNew split' },
+                { '<C-g>a', ":<C-u>'<,'>GpAppend<cr>", desc = 'Visual Append (after)' },
+                { '<C-g>b', ":<C-u>'<,'>GpPrepend<cr>", desc = 'Visual Prepend (before)' },
+                { '<C-g>c', ":<C-u>'<,'>GpChatNew<cr>", desc = 'Visual Chat New' },
+                { '<C-g>g', group = 'generate into new ..' },
+                { '<C-g>ge', ":<C-u>'<,'>GpEnew<cr>", desc = 'Visual GpEnew' },
+                { '<C-g>gn', ":<C-u>'<,'>GpNew<cr>", desc = 'Visual GpNew' },
+                { '<C-g>gp', ":<C-u>'<,'>GpPopup<cr>", desc = 'Visual Popup' },
+                { '<C-g>gt', ":<C-u>'<,'>GpTabnew<cr>", desc = 'Visual GpTabnew' },
+                { '<C-g>gv', ":<C-u>'<,'>GpVnew<cr>", desc = 'Visual GpVnew' },
+                { '<C-g>i', ":<C-u>'<,'>GpImplement<cr>", desc = 'Implement selection' },
+                { '<C-g>u', ":<C-u>'<,'>GpUnitTests<cr>", desc = 'Unit Tests' },
+                { '<C-g>e', ":<C-u>'<,'>GpExplain<cr>", desc = 'Explain' },
+                { '<C-g>f', ":<C-u>'<,'>GpFormatString<cr>", desc = 'Format String' },
+                { '<C-g>n', '<cmd>GpNextAgent<cr>', desc = 'Next Agent' },
+                { '<C-g>p', ":<C-u>'<,'>GpChatPaste<cr>", desc = 'Visual Chat Paste' },
+                { '<C-g>r', ":<C-u>'<,'>GpRewrite<cr>", desc = 'Visual Rewrite' },
+                { '<C-g>s', '<cmd>GpStop<cr>', desc = 'GpStop' },
+                { '<C-g>t', ":<C-u>'<,'>GpChatToggle<cr>", desc = 'Visual Toggle Chat' },
+                { '<C-g>x', ":<C-u>'<,'>GpContext<cr>", desc = 'Visual GpContext' },
+            },
+            -- NORMAL mode mappings
+            {
+                mode = { 'n' },
+                nowait = true,
+                remap = false,
+                { '<C-g><C-t>', '<cmd>GpChatNew tabnew<cr>', desc = 'New Chat tabnew' },
+                { '<C-g><C-v>', '<cmd>GpChatNew vsplit<cr>', desc = 'New Chat vsplit' },
+                { '<C-g><C-x>', '<cmd>GpChatNew split<cr>', desc = 'New Chat split' },
+                { '<C-g>a', '<cmd>GpAppend<cr>', desc = 'Append (after)' },
+                { '<C-g>b', '<cmd>GpPrepend<cr>', desc = 'Prepend (before)' },
+                { '<C-g>c', '<cmd>GpChatNew<cr>', desc = 'New Chat' },
+                { '<C-g>f', '<cmd>GpChatFinder<cr>', desc = 'Chat Finder' },
+                { '<C-g>g', group = 'generate into new ..' },
+                { '<C-g>ge', '<cmd>GpEnew<cr>', desc = 'GpEnew' },
+                { '<C-g>gn', '<cmd>GpNew<cr>', desc = 'GpNew' },
+                { '<C-g>gp', '<cmd>GpPopup<cr>', desc = 'Popup' },
+                { '<C-g>gt', '<cmd>GpTabnew<cr>', desc = 'GpTabnew' },
+                { '<C-g>gv', '<cmd>GpVnew<cr>', desc = 'GpVnew' },
+                { '<C-g>n', '<cmd>GpNextAgent<cr>', desc = 'Next Agent' },
+                { '<C-g>r', '<cmd>GpRewrite<cr>', desc = 'Inline Rewrite' },
+                { '<C-g>s', '<cmd>GpStop<cr>', desc = 'GpStop' },
+                { '<C-g>t', '<cmd>GpChatToggle<cr>', desc = 'Toggle Chat' },
+                { '<C-g>x', '<cmd>GpContext<cr>', desc = 'Toggle GpContext' },
+            },
         })
     end,
 }
