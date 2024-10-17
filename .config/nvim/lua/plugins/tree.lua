836f91826c15e9a8538326bfcda74ffe5405d0a1
Cenk Altı <cenkalti@gmail.com>
2024-10-17 14:55:01 -0400

    vim: fix nvim tree quit

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -48,17 +48,21 @@ return {
             -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#marvinth01
             vim.api.nvim_create_autocmd('QuitPre', {
                 callback = function()
-                    local invalid_win = {}
+                    local tree_wins = {}
+                    local floating_wins = {}
                     local wins = vim.api.nvim_list_wins()
                     for _, w in ipairs(wins) do
                         local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
                         if bufname:match('NvimTree_') ~= nil then
-                            table.insert(invalid_win, w)
+                            table.insert(tree_wins, w)
+                        end
+                        if vim.api.nvim_win_get_config(w).relative ~= '' then
+                            table.insert(floating_wins, w)
                         end
                     end
-                    if #invalid_win == #wins - 1 then
+                    if 1 == #wins - #floating_wins - #tree_wins then
                         -- Should quit, so we close all invalid windows.
-                        for _, w in ipairs(invalid_win) do
+                        for _, w in ipairs(tree_wins) do
                             vim.api.nvim_win_close(w, true)
                         end
                     end
