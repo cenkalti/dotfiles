e80208edfbf107fe25bec28ca9a94e7b077568c4
Cenk Altı <cenkalti@gmail.com>
2024-11-12 22:47:22 -0500

    vim: show symbols on auto-complete menu

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -20,6 +20,9 @@ return {
 
             -- For key bindings
             { 'folke/which-key.nvim' },
+
+            -- For icons in auto-complete menu
+            { 'onsails/lspkind.nvim' },
         },
 
         config = function()
@@ -105,6 +108,8 @@ return {
             cmp.setup({
                 sources = {
                     { name = 'nvim_lsp' },
+                    { name = 'nvim_lua' },
+                    { name = 'buffer' },
                 },
                 snippet = {
                     expand = function(args)
@@ -117,14 +122,32 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<Tab>'] = cmp.mapping.confirm(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
                     ['<C-c>'] = cmp.mapping.close(),
                     ['<C-n>'] = cmp.mapping.select_next_item(),
                     ['<C-p>'] = cmp.mapping.select_prev_item(),
                 }),
+                formatting = {
+                    -- changing the order of fields so the icon is the first
+                    fields = { 'abbr', 'kind', 'menu' },
+
+                    format = require('lspkind').cmp_format({
+                        mode = 'symbol_text',
+                        maxwidth = {
+                            -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                            -- can also be a function to dynamically calculate max width such as
+                            -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+                            menu = 50, -- leading text (labelDetails)
+                            abbr = 50, -- actual suggestion item
+                        },
+                        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+                    }),
+                },
             })
 
             -- Configure LSP diagnostic messages
