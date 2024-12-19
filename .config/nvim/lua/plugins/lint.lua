29a74e665b57ce7267cfba9e00bcfb1364ee1794
Cenk Altı <cenkalti@gmail.com>
2024-12-19 10:50:00 +0300

    vim: remove flake8 linter

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -8,7 +8,6 @@ return {
 
         require('lint').linters_by_ft = {
             markdown = { 'vale' },
-            python = { 'flake8' },
         }
 
         vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
