746b40a0ebc0b8586348b5b3a64901c1c9d661ac
Cenk Altı <cenkalti@gmail.com>
2024-12-22 13:03:12 +0300

    vim: remove unused local vars

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -22,7 +22,7 @@ return {
                 show_close_icon = false,
                 show_buffer_close_icons = false,
                 diagnostics = 'nvim_lsp',
-                diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                diagnostics_indicator = function(count, level)
                     local icon = level:match('error') and ' ' or ' '
                     return ' ' .. icon .. count
                 end,
