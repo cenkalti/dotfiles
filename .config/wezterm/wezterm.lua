24da4dc325277727a0b302f205399e39f3084f01
Cenk Altı <cenkalti@gmail.com>
2025-01-05 18:01:06 +0300

    wez: do not warn about missing glyphs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,6 +17,7 @@ config.enable_scroll_bar = true
 config.macos_window_background_blur = 10
 config.window_decorations = 'RESIZE'
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.warn_about_missing_glyphs = false
 
 -- Read ~/projects dir and add each dir to launch menu
 config.launch_menu = {}
