3feebb6bc4e26d7001f4f80ed87a4befdd8556bd
Cenk Altı <cenkalti@gmail.com>
2025-01-05 18:19:39 +0300

    vim: limit treesitter-context lines

--- .config/nvim/lua/plugins/treesitter-context.lua
+++ .config/nvim/lua/plugins/treesitter-context.lua
@@ -1,4 +1,10 @@
 -- Show context on top of the window
 return {
     'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+        require('treesitter-context').setup({
+            multiline_threshold = 1, -- Maximum number of lines to show for a single context
+            mode = 'topline', -- Line used to calculate context. Choices: 'cursor', 'topline'
+        })
+    end,
 }
