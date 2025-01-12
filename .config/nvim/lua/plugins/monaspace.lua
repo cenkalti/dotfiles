c55b988fa197de1209a5c94578dfde8ff0383e32
Cenk Altı <cenkalti@gmail.com>
2025-01-12 12:26:24 +0300

    Revert "vim: install monaspace font"
    This reverts commit dca82e63fdebc756304b7c357a2394ef306e1fcc.

--- .config/nvim/lua/plugins/monaspace.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'jackplus-xyz/monaspace.nvim',
-    lazy = false,
-    opts = {
-        -- Use the preconfigured highlight group setting
-        -- Set this to `false` if you want to make your own config from scratch
-        use_default = true,
-    },
-}
