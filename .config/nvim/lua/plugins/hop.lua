2ea5dcb1ddfbcd09749529e755f7797a3c0ea1d6
Cenk Altı <cenkalti@gmail.com>
2025-02-22 23:39:38 +0300

    vim: update hop plugin config

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -1,18 +1,16 @@
 -- Search with 2 letters
 return {
-    {
-        'smoka7/hop.nvim',
-        version = '*',
-        dependencies = {
-            { 'folke/which-key.nvim' },
-        },
-        config = function()
-            local hop = require('hop')
-            local wk = require('which-key')
-            hop.setup()
-            wk.add({
-                { 's', "<cmd>lua require('hop').hint_char2()<cr>", desc = 'Hop 2 Chars' },
-            })
-        end,
+    'smoka7/hop.nvim',
+    version = '*',
+    dependencies = {
+        { 'folke/which-key.nvim' },
     },
+    config = function()
+        local hop = require('hop')
+        local wk = require('which-key')
+        hop.setup()
+        wk.add({
+            { 's', "<cmd>lua require('hop').hint_char2()<cr>", desc = 'Hop 2 Chars' },
+        })
+    end,
 }
