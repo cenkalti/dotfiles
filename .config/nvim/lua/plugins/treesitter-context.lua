fb8addb517216d208190187358385218a3398fb5
Cenk Altı <cenkalti@gmail.com>
2025-03-24 23:34:54 -0400

    vim: enable treesitter context on multiple windows

--- .config/nvim/lua/plugins/treesitter-context.lua
+++ .config/nvim/lua/plugins/treesitter-context.lua
@@ -3,6 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     config = function()
         require('treesitter-context').setup({
+            multiwindow = true, -- Show context in multiple windows
             multiline_threshold = 1, -- Maximum number of lines to show for a single context
             mode = 'topline', -- Line used to calculate context. Choices: 'cursor', 'topline'
             max_lines = 2, -- How many lines the window should span. Values <= 0 mean no limit
