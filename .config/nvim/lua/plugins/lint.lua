804d74d9868e00bd9424ca440d3d1ac034fb7b36
Cenk Altı <cenkalti@gmail.com>
2025-04-11 11:07:16 -0400

    Revert "vim: remove linter plugin"
    This reverts commit 2e31158eef190c20e7d7dc19162f52c98d662d0a.

---
+++ .config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,20 @@
+return {
+    'mfussenegger/nvim-lint',
+    config = function()
+        local venv = os.getenv('VIRTUAL_ENV')
+        if venv then
+            require('lint').linters.mypy.args = { '--config-file', venv .. '/mypy.ini' }
+        end
+
+        require('lint').linters_by_ft = {
+            markdown = { 'vale' },
+        }
+
+        vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+            callback = function()
+                -- try_lint without arguments runs the linters defined in `linters_by_ft` for the current filetype
+                require('lint').try_lint()
+            end,
+        })
+    end,
+}
