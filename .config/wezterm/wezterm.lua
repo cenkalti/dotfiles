8e0f2a3fc5e2b748d8ce92ad53caac1d689c63ee
Cenk Altı <cenkalti@gmail.com>
2025-04-28 10:14:02 -0400

    Revert "wez: use retry-style tab bar"
    This reverts commit 6bb2a82130ea5160c2aea0b308b238d64949c321.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,14 +9,13 @@ config.font_size = 15.0
 config.quit_when_all_windows_are_closed = false
 config.default_prog = { '/opt/homebrew/bin/zsh', '-li' }
 config.adjust_window_size_when_changing_font_size = false
-config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 120
 config.initial_rows = 36
 config.scrollback_lines = 100000
 config.enable_scroll_bar = true
 config.macos_window_background_blur = 10
-config.window_decorations = 'RESIZE | MACOS_FORCE_SQUARE_CORNERS'
+config.window_decorations = 'RESIZE'
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.warn_about_missing_glyphs = false
 config.window_content_alignment = {
