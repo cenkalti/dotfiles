3a742e383b50607fb4d8580246cbb754d0373e9e
Cenk Altı <cenkalti@gmail.com>
2025-06-05 16:57:30 -0400

    nvim: add ActivityWatch plugin

---
+++ .config/nvim/lua/plugins/activity-watch.lua
@@ -0,0 +1,10 @@
+-- ActivityWatch client for Neovim
+return {
+    'lowitea/aw-watcher.nvim',
+    opts = {
+        aw_server = {
+            host = '127.0.0.1',
+            port = 5600,
+        },
+    },
+}
