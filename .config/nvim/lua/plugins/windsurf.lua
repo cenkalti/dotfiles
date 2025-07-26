8053f2450b694635095251a6516bb9ca528f5d18
Cenk Altı <cenkalti@gmail.com>
2025-07-26 01:04:38 -0400

    nvim: update ai completion mappings

--- .config/nvim/lua/plugins/windsurf.lua
+++ .config/nvim/lua/plugins/windsurf.lua
@@ -4,9 +4,17 @@ return {
     event = 'VeryLazy',
     config = function()
         local neocodeium = require('neocodeium')
-        neocodeium.setup()
+        neocodeium.setup({
+            silent = true,
+            filter = function(bufnr)
+                if vim.endswith(vim.api.nvim_buf_get_name(bufnr), '.env') then
+                    return false
+                end
+                return true
+            end,
+        })
 
-        vim.keymap.set('i', '<Tab>', function()
+        vim.keymap.set('i', '<A-y>', function()
             neocodeium.accept()
         end)
         vim.keymap.set('i', '<A-w>', function()
@@ -15,13 +23,16 @@ return {
         vim.keymap.set('i', '<A-l>', function()
             neocodeium.accept_line()
         end)
-        vim.keymap.set('i', '<A-j>', function()
+        vim.keymap.set('i', '<A-Space>', function()
             neocodeium.cycle_or_complete(1)
         end)
-        vim.keymap.set('i', '<A-k>', function()
-            neocodeium.cycle_or_complete(-1)
+        vim.keymap.set('i', '<A-n>', function()
+            neocodeium.cycle(1)
         end)
-        vim.keymap.set('i', '<A-c>', function()
+        vim.keymap.set('i', '<A-p>', function()
+            neocodeium.cycle(-1)
+        end)
+        vim.keymap.set('i', '<A-e>', function()
             neocodeium.clear()
         end)
     end,
