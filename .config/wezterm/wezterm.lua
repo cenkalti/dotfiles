3833464570689ce0c35a653080664f8fb91cd097
Cenk Altı <cenkalti@gmail.com>
2025-07-29 12:25:25 -0400

    wez: remove resurrect

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,9 +7,6 @@ wezterm.on('update-right-status', function(window, pane)
     window:set_right_status(window:active_workspace())
 end)
 
-local resurrect = wezterm.plugin.require('https://github.com/MLFlexer/resurrect.wezterm')
-resurrect.state_manager.set_max_nlines(5000)
-
 -- This will hold the configuration.
 ---@class Config
 local config = wezterm.config_builder()
@@ -140,59 +137,23 @@ config.keys = {
         }),
     },
 
-    -- Save Workspace State
+    -- Rename Workspace
     {
-        key = 's',
-        mods = 'LEADER',
-        ---@diagnostic disable-next-line: unused-local, assign-type-mismatch
-        action = wezterm.action_callback(function(win, pane)
-            resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
-        end),
-    },
-    -- Restore Workspace State
-    {
-        key = 'r',
-        mods = 'LEADER',
-        ---@diagnostic disable-next-line: assign-type-mismatch
-        action = wezterm.action_callback(function(win, pane)
-            --- @diagnostic disable-next-line: unused-local
-            resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
-                local type = string.match(id, '^([^/]+)') -- match before '/'
-                id = string.match(id, '([^/]+)$') -- match after '/'
-                id = string.match(id, '(.+)%..+$') -- remove file extention
-                local opts = {
-                    relative = true,
-                    restore_text = true,
-                    on_pane_restore = resurrect.tab_state.default_on_pane_restore,
-                }
-                if type == 'workspace' then
-                    local state = resurrect.state_manager.load_state(id, 'workspace')
-                    resurrect.workspace_state.restore_workspace(state, opts)
-                elseif type == 'window' then
-                    local state = resurrect.state_manager.load_state(id, 'window')
-                    resurrect.window_state.restore_window(pane:window(), state, opts)
-                elseif type == 'tab' then
-                    local state = resurrect.state_manager.load_state(id, 'tab')
-                    resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+        key = 'R',
+        mods = 'SUPER',
+        action = wezterm.action.PromptInputLine({
+            description = wezterm.format({
+                { Attribute = { Intensity = 'Bold' } },
+                { Foreground = { AnsiColor = 'Fuchsia' } },
+                { Text = 'Rename workspace' },
+            }),
+            action = wezterm.action_callback(function(window, pane, line)
+                if line then
+                    ---@diagnostic disable-next-line: param-type-mismatch
+                    window:perform_action(wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line), pane)
                 end
-            end)
-        end),
-    },
-    -- Delete Workspace State
-    {
-        key = 'd',
-        mods = 'LEADER',
-        ---@diagnostic disable-next-line: assign-type-mismatch
-        action = wezterm.action_callback(function(win, pane)
-            resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id)
-                resurrect.state_manager.delete_state(id)
-            end, {
-                title = 'Delete State',
-                description = 'Select State to Delete and press Enter = accept, Esc = cancel, / = filter',
-                fuzzy_description = 'Search State to Delete: ',
-                is_fuzzy = true,
-            })
-        end),
+            end),
+        }),
     },
 }
 
