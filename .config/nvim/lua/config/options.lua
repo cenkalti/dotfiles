f5c6a203554d3c83f7f9e0ea2d95b26289e3b910
Cenk Altı <cenkalti@gmail.com>
2025-09-03 07:43:54 -0400

    nvim: remove fillchars option

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,7 +6,6 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.colorcolumn = '80,120'
 vim.opt.synmaxcol = 240
-vim.opt.fillchars = { vert = ' ' } -- Hide vertical fill characters between windows
 vim.opt.scrolloff = 5
 vim.opt.sessionoptions = 'buffers,curdir,folds'
 vim.opt.foldmethod = 'marker'
