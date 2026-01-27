d259526af0f94b530a160dc58d1513ea5a86c6c0
Jake Alti <cenk.alti@goteleport.com>
2026-01-27 00:19:14 -0500

    nvim: update lualine components

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -15,8 +15,8 @@ return {
                 },
                 sections = {
                     lualine_a = { 'mode' },
-                    lualine_b = {},
-                    lualine_c = {},
+                    lualine_b = { 'branch' },
+                    lualine_c = { { 'filename', path = 1 } },
                     lualine_x = {},
                     lualine_y = { 'diff' },
                     lualine_z = { number_of_lines },
