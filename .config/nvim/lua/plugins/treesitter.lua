95c44642a6a5f3de85ea40fdb9f06ea71dfc58da
Jake Alti <cenk.alti@goteleport.com>
2026-01-28 13:26:29 -0500

    nvim: add class targets for treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -66,6 +66,7 @@ return {
                         goto_next_start = {
                             [']f'] = '@function.outer',
                             [']i'] = '@function.inner',
+                            [']t'] = '@class.outer',
                         },
                         goto_next_end = {
                             [']F'] = '@function.outer',
@@ -74,6 +75,7 @@ return {
                         goto_previous_start = {
                             ['[f'] = '@function.outer',
                             ['[i'] = '@function.inner',
+                            ['[t'] = '@class.outer',
                         },
                         goto_previous_end = {
                             ['[F'] = '@function.outer',
