1f4dee1eac2684c5e32e4421fd81d90b2b013838
Jake Alti <cenk.alti@goteleport.com>
2026-03-24 21:45:15 -0400

    nvim: update bookmark keybindings

--- .config/nvim/lua/plugins/bookmarks.lua
+++ .config/nvim/lua/plugins/bookmarks.lua
@@ -10,15 +10,10 @@ return {
 
         local bookmarks = require('lspmark.bookmarks')
 
-        vim.keymap.set('n', 'mm', function()
-            bookmarks.toggle_bookmark({ with_comment = false })
-        end, { desc = 'Toggle bookmark' })
+        vim.keymap.set('n', 'mm', bookmarks.toggle_bookmark, { desc = 'Toggle bookmark' })
         vim.keymap.set('n', 'mc', bookmarks.modify_comment, { desc = 'Modify bookmark comment' })
         vim.keymap.set('n', 'ms', bookmarks.show_comment, { desc = 'Show bookmark comment' })
         vim.keymap.set('n', 'ml', '<cmd>Telescope lspmark<cr>', { desc = 'List bookmarks' })
-        vim.keymap.set('n', 'dd', bookmarks.delete_line, { desc = 'Delete line (bookmark-aware)' })
-        vim.keymap.set('x', 'd', bookmarks.delete_visual_selection, { desc = 'Delete selection (bookmark-aware)' })
-        vim.keymap.set('n', 'p', bookmarks.paste_text, { desc = 'Paste (bookmark-aware)' })
 
         -- Load bookmarks on startup and when changing directories
         bookmarks.load_bookmarks()
