5cee584d90406d16fe46cefe683275be8cc79ca2
Jake Alti <cenk.alti@goteleport.com>
2026-04-09 14:46:18 -0400

    nvim: replace-with-register plugin replacement

--- .config/nvim/lua/plugins/replace-with-register.lua
+++ .config/nvim/lua/plugins/replace-with-register.lua
@@ -1,4 +1,28 @@
--- Replace with yanked text: `grw`
-return {
-    'inkarkat/vim-ReplaceWithRegister',
-}
+-- Replace with register: `cr{motion}` (e.g., `crw`, `criw`, `cr$`)
+-- Replaces vim-ReplaceWithRegister plugin (gr conflicts with neovim 0.12 defaults)
+
+-- cr{motion}: set our function as the operator, g@ waits for the motion
+vim.keymap.set('n', 'cr', function()
+  vim.o.operatorfunc = 'v:lua.__replace_with_register'
+  return 'g@'
+end, { expr = true })
+
+-- crr: replace entire line (g@_ applies the operator to the current line)
+vim.keymap.set('n', 'crr', function()
+  vim.o.operatorfunc = 'v:lua.__replace_with_register'
+  return 'g@_'
+end, { expr = true })
+
+-- visual mode: paste over selection
+vim.keymap.set('x', 'cr', function()
+  vim.cmd('normal! "' .. vim.v.register .. 'p')
+end)
+
+-- operatorfunc callback: visually select the motion range (`[..`]) and paste over it
+function __replace_with_register(type)
+  local reg = vim.v.register
+  local sel = type == 'line' and 'V' or type == 'block' and '\22' or 'v'
+  vim.cmd('normal! `[' .. sel .. '`]"' .. reg .. 'p')
+end
+
+return {}
