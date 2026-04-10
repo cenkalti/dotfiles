d6d66bed847d44f33005a6c0d13f9f453e020395
Jake Alti <cenk.alti@goteleport.com>
2026-04-10 16:38:11 -0400

    nvim: replace vim-illuminate

--- .config/nvim/lua/plugins/illuminate.lua
+++ .config/nvim/lua/plugins/illuminate.lua
@@ -1,4 +1,22 @@
--- Highlight word under cursor
-return {
-    'RRethy/vim-illuminate',
-}
+-- Highlight word under cursor via LSP document highlights
+
+local group = vim.api.nvim_create_augroup('WordHighlight', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+    group = group,
+    callback = function()
+        local clients = vim.lsp.get_clients({ bufnr = 0, method = 'textDocument/documentHighlight' })
+        if #clients > 0 then
+            vim.lsp.buf.document_highlight()
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+    group = group,
+    callback = function()
+        vim.lsp.buf.clear_references()
+    end,
+})
+
+return {}
