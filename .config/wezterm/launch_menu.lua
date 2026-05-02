daa0b5784007fb93319d8192eba4838a861ecef7
Jake Alti <cenk.alti@goteleport.com>
2026-05-02 15:12:45 -0400

    wez: refactor launch menu

--- .config/wezterm/launch_menu.lua
+++ .config/wezterm/launch_menu.lua
@@ -1,27 +1,24 @@
+local wezterm = require('wezterm')
+
 local M = {}
 
 function M.setup(config)
-    -- Read ~/projects dir and add each dir to launch menu
     config.launch_menu = {}
-    local workspace_dirs = { '~/projects', '~/.config' }
+    local workspace_dirs = { wezterm.home_dir .. '/projects', wezterm.home_dir .. '/.config' }
     for _, workspace_dir in ipairs(workspace_dirs) do
-        local handle = io.popen('ls -1 ' .. workspace_dir)
-        if handle then
-            for project_dir in handle:lines() do
+        for _, entry in ipairs(wezterm.read_dir(workspace_dir)) do
+            local basename = entry:match('([^/]+)$')
+            if basename and not basename:match('^%.') then
                 table.insert(config.launch_menu, {
-                    label = project_dir,
+                    label = basename,
                     args = {
                         '/opt/homebrew/bin/zsh',
                         '-lic',
-                        'cd '
-                            .. workspace_dir
-                            .. '/'
-                            .. project_dir
-                            .. ' && exec /opt/homebrew/bin/nvim -c "Telescope find_files"',
+                        'exec /opt/homebrew/bin/nvim -c "Telescope find_files"',
                     },
+                    cwd = entry,
                 })
             end
-            handle:close()
         end
     end
 end
