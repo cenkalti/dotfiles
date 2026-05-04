837fd170e75895c4bd258681a5972888848dc46a
Jake Alti <cenk.alti@goteleport.com>
2026-05-03 23:29:54 -0400

    wez: increase window frame font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -34,6 +34,10 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Catppuccin Macchiato'
 config.font = wezterm.font('Monaspace Argon NF')
 config.font_size = 16.0
+config.window_frame = {
+    font = wezterm.font('Monaspace Argon NF'),
+    font_size = 16.0,
+}
 config.quit_when_all_windows_are_closed = false
 config.default_prog = { '/opt/homebrew/bin/zsh', '-li' }
 config.adjust_window_size_when_changing_font_size = false
