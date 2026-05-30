3ce4729e37f705c1219bc89c7f991404647dd510
Jake Alti <cenk.alti@goteleport.com>
2026-05-30 16:35:39 -0400

    wez: stringify quake tab key for GLOBAL

--- .config/wezterm/quake.lua
+++ .config/wezterm/quake.lua
@@ -7,8 +7,8 @@ function M.setup(_)
         wezterm.GLOBAL.quake_panes = wezterm.GLOBAL.quake_panes or {}
         local quake_panes = wezterm.GLOBAL.quake_panes
         local tab = window:active_tab()
-        local tab_id = tab:tab_id()
-        local existing_id = quake_panes[tab_id]
+        local tab_key = tostring(tab:tab_id())
+        local existing_id = quake_panes[tab_key]
 
         if existing_id then
             for _, info in ipairs(tab:panes_with_info()) do
@@ -18,18 +18,18 @@ function M.setup(_)
                         wezterm.action.CloseCurrentPane({ confirm = false }),
                         info.pane
                     )
-                    quake_panes[tab_id] = nil
+                    quake_panes[tab_key] = nil
                     return
                 end
             end
-            quake_panes[tab_id] = nil
+            quake_panes[tab_key] = nil
         end
 
         local new_pane = pane:split({
             direction = 'Bottom',
             size = 0.2,
         })
-        quake_panes[tab_id] = new_pane:pane_id()
+        quake_panes[tab_key] = new_pane:pane_id()
     end)
 end
 
