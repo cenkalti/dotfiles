5aecc81b36c529df26cb338e7b5a0245ec2199ba
Jake Alti <cenk.alti@goteleport.com>
2026-06-16 16:37:28 +0300

    nvim: add keybinding to toggle render-markdown

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,12 +1,15 @@
 -- Render markdown in normal mode, raw while editing
 return {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons', 'folke/which-key.nvim' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     opts = {
         anti_conceal = { enabled = false },
     },
     config = function(_, opts)
         require('render-markdown').setup(opts)
+        require('which-key').add({
+            { '<leader>m', '<cmd>RenderMarkdown toggle<CR>', desc = 'Toggle Render Markdown' },
+        })
     end,
 }
